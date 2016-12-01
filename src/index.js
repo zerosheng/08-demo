f73e07617b6867f7f36a0e354f1c2d2f7ee74d15
zerosheng <403826017@qq.com>
2016-12-01 16:25:14 +0800

    deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-import Routers from './routes.js';
-import './main.css';
-
-render(<Routers />,document.getElementById('root'));
