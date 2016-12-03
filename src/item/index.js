945417d2853010ae85656ae12acafdfb92b49de2
zerosheng <403826017@qq.com>
2016-12-03 15:13:28 +0800

    blog2

--- src/item/index.js
+++ src/item/index.js
@@ -3,9 +3,9 @@ import React, { PropTypes } from 'react'
 class Item extends React.Component {
   render () {
     console.log(this.props)
-    let content=this.props.params.title==1? '这是第1个页面' :
-                this.props.params.title==2? '这是第2个页面' :
-                this.props.params.title==3? '这是第3个页面' : '这是第n个页面'
+    let content=this.props.params.title==0? '这是第1个页面' :
+                this.props.params.title==1? '这是第2个页面' :
+                this.props.params.title==2? '这是第3个页面' : '这是第n个页面'
      return(
        <div>
        {content}
