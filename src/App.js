b3b9fa3dc58f9c2bbad891d6d71193ae425d54cb
zerosheng <403826017@qq.com>
2016-12-04 12:03:11 +0800

    1204 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavHeader from './component/NavHeader';
 import NavFooter from './component/NavFooter';
-
+import LeftNav from './component/LeftNav';
 class App extends React.Component {
   constructor(){
     super();
-    this.state={title:'Home'}
+    this.state={
+      title:'Home',
+      showLeftNav:false
+    }
   }
   getChildContext(){
     return{muiTheme:getMuiTheme()}
@@ -17,6 +20,15 @@ class App extends React.Component {
   }
   componentWillMount(){
     this.setTitle();
+    this.setNavBar();
+    }
+  setNavBar(){
+    this.setState({
+      showLeftNav:window.innerWidth>760 ? true:false
+    })
+  }
+  componentDidMount(){
+    window.onresize=this.setNavBar.bind(this)
   }
   setTitle(){
     this.setState({
@@ -30,13 +42,13 @@ class App extends React.Component {
     // console.log(this.props);
     return(
       <div className='my-wrap'>
-        <NavHeader title={this.state.title}/>
+        {this.state.showLeftNav?<LeftNav title={this.state.title}/>:<NavHeader title={this.state.title}/>}
 
         <div className='main'>
           {this.props.children}
         </div>
 
-        <NavFooter />
+        {this.state.showLeftNav?null:<NavFooter />}
       </div>
     )
   }
